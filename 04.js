fe275262e80775bc566bbcc2aa05a5179d088203
GianfrancoFrau <gian.frau@hotmail.it>
2015-09-19 11:46:15 +0200

    ex 4

--- 04.js
+++ 04.js
@@ -2,6 +2,14 @@ var q = require('q');
 // create a promise
 var deferred = q.defer();
 
-deferred.promise.then(console.log);
+// USING Q
+// console.log('FIRST');
+// deferred.promise.then(console.log);
+// deferred.resolve('SECOND');
 
-deferred.resolve('SECOND');
+// USING JS
+var p = new Promise(function(resolve, reject){
+	resolve('SECOND');
+});
+p.then(console.log);
+console.log('FIRST');
